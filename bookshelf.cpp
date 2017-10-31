232da0d51838a98789c3367488838aa01b8a63e9
Jonathan McMillan <jonathan.mcmillan@slu.edu>
2017-10-31 13:19:25 -0500

    Added file bookshelf.cpp, not complete answer yet though.

--- bookshelf.cpp
+++ bookshelf.cpp
@@ -1,17 +1,92 @@
 #include <iostream>
 #include <list>
-#include <vector>
+#include <algorithm>
 using namespace std;
+list<int> bookNumbers;
+list<int> bookSizes;
+int currentSize = 0;
+
+void addBook(int bookNumber, int bookSize, int shelfWidth) {
+	cout << "Entered addBook" << endl;
+	currentSize = currentSize + bookSize;
+	while (currentSize > shelfWidth) {
+		currentSize = currentSize - bookSizes.front();
+		bookSizes.pop_front();
+		bookNumbers.pop_front();
+	}
+	bookSizes.push_back(bookSize);
+	bookNumbers.push_back(bookNumber);
+	cout << "Added " << bookNumber << endl;
+}
+
+void removeBook(int bookNumber) {
+	cout << "Entered removeBook" << endl;
+	list<int>::iterator it1, it2;
+	it1 = bookSizes.begin();
+	it2 = bookNumbers.begin();
+
+	if(find(bookNumbers.begin(), bookNumbers.end(), bookNumber) != bookNumbers.end()){
+		while (true) {
+			if (*it1 == bookNumber) {
+				it1 = bookNumbers.erase(it1);
+				currentSize = currentSize - *it2;
+				it2 = bookSizes.erase(it2);
+				break;
+			}
+		 	if(*it1 == bookNumbers.back()){
+				break;
+			}
+			else{
+				it1++;
+				it2++;
+			}
+		}
+	}
+	cout << "Left removeBooks" << endl;
+}
+
+void printReset() {
+	cout << "Entered printReset" << endl;
+	while (!bookNumbers.empty()) {
+		cout << bookNumbers.back();
+		bookNumbers.pop_back();
+		bookSizes.pop_back();
+	}
+	currentSize = 0;
+}
 
 int main() {
-
-    int shelfWidth;
-    while (cin >> shelfWidth && shelfWidth != -1) {
-        list<int> bookshelf;          // IDs for the books currently on the shelf, from left to right
-        int total(0);                 // cumulative width of all books on the shelf
-        vector<int> width(101, 0);    // width for known books, indexed by book ID
-        vector<list<int>::iterator> locate(101, bookshelf.end());    // positions of books on the shelf, index by ID
-
-        // run the simulation and output the results...
-    }
+	int numTimes;
+	while (true) {
+		int shelfWidth;
+		cin >> shelfWidth;
+		numTimes++;
+		if (shelfWidth == -1) {
+			break;
+		}
+		else {
+			while (true) {
+				string input;
+				cin >> input;
+				if (input == "E") {
+					cout << "PROBLEM " << numTimes << ": ";
+					printReset();
+					break;
+				}
+				else {
+					int bookNumber;
+					cin >> bookNumber;
+					if (input == "R") {
+						removeBook(bookNumber);
+					}
+					else if (input == "A") {
+						int bookSize;
+						cin >> bookSize;
+						addBook(bookNumber, bookSize, shelfWidth);
+					}
+				}
+			}
+		}
+	}
+	return 0;
 }
