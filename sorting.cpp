903f44a53e3a110f5b2158cb467f7f955b10d981
Jonathan McMillan <jonathan.mcmillan@slu.edu>
2017-11-07 09:03:34 -0600

    Added bubble sort to sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -2,6 +2,18 @@
 #include <algorithm>
 using namespace std;
 
+
+void BubbleSort([] A,int n ){
+        for(int i = 0; i < n-1; i++){
+                int iMin = i;
+                for(int j = 0; j< n-i-1; j++){
+                        if(A[j] >  A[i+1]){
+                                swap(A[i], A[i+1]);
+                        }
+                }
+        }
+}
+
 int main()
 {
     int A[] = {2, 7, 4, 1, 5, 3};
