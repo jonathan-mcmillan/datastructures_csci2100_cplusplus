b615ba602f1db2bc31adc15e30090a2a68a5717b
Jonathan McMillan <jonathan.mcmillan@slu.edu>
2017-12-05 13:36:13 -0600

    Added LinkedBinaryTree.* and testLinkedBinaryTree.cpp

---
+++ LinkedBinaryTree.h
@@ -0,0 +1,63 @@
+#ifndef LINKEDBINARYTREE_H
+#define LINKEDBINARYTREE_H
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+template <typename Object>
+class LinkedBinaryTree{
+
+protected:
+	struct Node{
+		Object elem;
+		Node* parent;
+		Node* left;
+		Node* right;
+		Node() : elem(), parent(NULL), left(NULL), right(NULL) { }
+		Node(Object& e) : elem(e), parent(NULL), left(NULL), right(NULL) { }
+	};
+
+public:
+	class Position{
+	private:
+		Node* v;
+	public:
+		Position(Node* _v = NULL) : v(_v) { }
+		Object& operator*() { return v -> elem; }
+		Position left() const { return Position(v->left); }
+		Position right() const { return Position(v->right); }
+		Position parent() const { return Position(v->parent); }
+		bool isRoot() const { return v->parent == NULL; }
+		bool isExternal() const { return v->left == NULL && v->right == NULL; }
+		bool isNULL() { return (v == NULL); }
+		friend class LinkedBinaryTree;
+	};
+
+public:
+	LinkedBinaryTree();
+	int size() const;
+	bool empty() const;
+	Position root() const;
+	void addRoot(const Object& value = Object());
+	void addLeftLeaf(const Position& p, const Object& value);
+	void addRightLeaf(const Position& p, const Object& value);
+	void preorderPrint() const;
+	void inorderPrint() const;
+	void postorderPrint() const;
+	void expandExternal(const Position& p);
+	Position removeAboveExteranl(const Position& p);
+
+protected:
+
+private:
+	void preorder(const Node* v)const;
+	void inorder(const Node* v) const;
+	void postorder(const Node* v) const;
+	Node* _root;
+	int n;
+};
+
+#include "LinkedBinaryTree.cpp"
+#endif
---
+++ testLinkedBinaryTree.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "LinkedBinaryTree.h"
+
+int main(){
+	LinkedBinaryTree<int> tree1;
+	LinkedBinaryTree<int> tree2;
+
+	tree1.addRoot(9);
+	tree1.addLeftLeaf(tree1.root(), 7);
+	tree1.addRightLeaf(tree1.root(), 11);
+	tree1.addLeftLeaf(tree1.root().left(), 3);
+	tree1.addRightLeaf(tree1.root().left(), 8);
+	tree1.addLeftLeaf(tree1.root().right(), 10);
+	tree1.addRightLeaf(tree1.root().right(), 14);
+	tree1.addLeftLeaf(tree1.root().left().left(), 2);
+	tree1.addRightLeaf(tree1.root().left().left(), 5);
+	
+	tree2.addRoot(9);
+        tree2.addLeftLeaf(tree2.root(), 7);
+        tree2.addRightLeaf(tree2.root(), 11);
+        tree2.addLeftLeaf(tree2.root().left(), 3);
+        tree2.addRightLeaf(tree2.root().left(), 8);
+        tree2.addLeftLeaf(tree2.root().right(), 10);
+        tree2.addRightLeaf(tree2.root().right(), 14);
+        tree2.addLeftLeaf(tree2.root().left().left(), 2);
+        tree2.addRightLeaf(tree2.root().left().left(), 100);
+
+	cout << "Preorder tree 1: " << endl;
+	tree1.preorderPrint();
+	
+	cout << endl <<  "Preorder tree 2: " << endl;
+	tree2.preorderPrint();
+	
+	cout << endl << "Inorder tree 1: " << endl;
+	tree1.inorderPrint();
+	
+	cout << endl << "Inorder tree 2: " << endl;
+	tree2.inorderPrint();
+
+	cout << endl << "Postorder tree 1: " << endl;
+	tree1.postorderPrint();
+	
+	cout << endl << "Postorder tree 2: " << endl;
+	tree2.postorderPrint();
+}
