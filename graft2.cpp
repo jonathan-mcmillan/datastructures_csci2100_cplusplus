afe88fef5066ad4bd4a1e4eb18fefd9edd3027d1
Jonathan McMillan <jonathan.mcmillan@slu.edu>
2017-12-07 00:04:32 -0600

    Added completed solution to lab 11, graft2.cpp

--- graft2.cpp
+++ graft2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
+
+string modInput = "";
 
 int getDepth(string input){
 	int num = 0;
@@ -18,13 +21,45 @@ int getDepth(string input){
         }
         return Depth;
 }
-
-string getModinput(string input){
-	string modInput = "";
-	
-	return modInput;	
+void getModInput(string input){
+	for(int i = 0; i < input.size(); i++){
+		if(input[i] == 'd')
+                	continue;
+                else if(input[i] == 'u'){
+                        if(input[i+1] == 'd'){
+                        	input.replace(i, 2, "du");
+                                i = i-1;
+                        }
+                        else if(input[i+1] == 'u')
+                               	continue;
+                }
+	}
+        modInput = input;
+        return;
 }
-
+/*
+void getModInput(string input, int i){
+	cout << "input: " << input << endl;
+	if(i == input.size()){
+	        cout << "setting modInput to input" << endl;
+	        modInput = input;
+       		cout << "input: " << input << " modInput: " << modInput << endl;
+	        return;
+	}
+	else{
+		if(input[i] == 'd')
+			getModInput(input, i+1);
+		else if(input[i] == 'u'){
+			if(input[i+1] == 'd'){
+				input.replace(i, 2, "du");
+				getModInput(input, i);
+			}
+			else if(input[i+1] == 'u')
+				getModInput(input, i+1);
+		}
+	}
+}
+*/
 int main(){
         bool tf = true;
         int numTrial = 1;
@@ -35,8 +70,10 @@ int main(){
                         tf = false;
                 }
                 else{
-                        cout << "Tree " << numTrial << ": " << getInitDepth(input) << "=>" << getDepth(getModInput(input)) << endl;
+			getModInput(input);
+                        cout << "Tree " << numTrial << ": " << getDepth(input) << "=>" << getDepth(modInput) << endl;
                         numTrial++;
+			modInput = "";
                 }
         }
 
